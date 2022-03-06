0261fe1a460e0c8e9eda2163df83ff2ac7879b7a
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2022-03-06 20:52:55 +0900

    Initial Commit

---
+++ RoadToLegends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RoadToLegends
+//
+//  Created by 黒川龍之介 on 2022/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
